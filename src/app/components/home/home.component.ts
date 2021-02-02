1a882ecfb368189a1fcbcc957dd61b6384d01f16
praveen683 <praveen.rv87@gmail.com>
2021-02-02 10:17:56 -0500

    implemented routerNaviagtion between home and notepad to get id

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NotepadService } from 'src/app/services/notepad.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
+  notePads = [];
+  gistAllFiles;
+  objectValues = Object.values;
 
-  constructor() { }
+  constructor(private router: Router, private notepadService: NotepadService) { }
 
   ngOnInit(): void {
+  }
+
+  addNotepad() {
+    this.router.navigate(['notepad']);
+  }
+
+  viewNotepad(notePad) {
+    this.router.navigate(['notepad'], { queryParams: { id: notePad.id } });
+  }
+  
+  async allGist() {
+    let res = await this.notepadService.getAllGist();
+    this.gistAllFiles = res.data;
+    for (let gistFile of this.gistAllFiles) {
+      if (gistFile && gistFile.files && Object.values(gistFile.files).length) {
+        let notePad = {};
+        let gistVal: any = Object.values(gistFile.files)[0];
+        notePad['id'] = gistFile.id;
+        notePad['filename'] = gistVal.filename;
+        notePad['name'] = gistVal.filename.split('.')[0];
+        this.notePads.push(notePad);
+      }
+    }
   }
 
 }
